5b746ce8dfe15c715d4200cbc593c848b680c54d
connor <connorzhao720@gmail.com>
2026-02-07 21:27:03 -0800

    Create TeamMemberPractice.java
    bet

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamMemberPractice.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class TeamMemberPractice extends OpMode {
+    @Override
+    public void loop() {
+        double yAxis = gamepad1.left_stick_y;
+
+        telemetry.addData("left stick Normal", yAxis);
+
+        yAxis = squareInputWithSign(yAxis);
+
+        telemetry.addData("left stick modified", yAxis);
+    }
+    double squareInputWithSign(double input){
+        double output = input * input;
+        if (input < 0){
+            output *=-1;
+        }
+        return output;
+    }
+    @Override
+    public void init() {
+
+    }
+}
